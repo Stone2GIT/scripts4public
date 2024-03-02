6834ae29f144d6593461b15e61e081a95d3283bd
Dietmar Stein <info@simracingjustfair.org>
2024-03-02 22:16:13 +0100

    some notes

--- rfactor2/pwsh/gui/installer.ps1
+++ rfactor2/pwsh/gui/installer.ps1
@@ -2,3 +2,4 @@
 # just a placeholder atm
 #
 # ModMgr.exe -i"AstonMartin_Vantage_GT3_2019_v3.60.rfcmp" -p"[packages folder]" -d"[rf2root folder]"
+#  gci .\2103827617\ *.rfcmp -recurse| select -Expand Name|sort
