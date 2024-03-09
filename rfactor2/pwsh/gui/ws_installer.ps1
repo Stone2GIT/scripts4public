a3c4ab7c790ebed29d87aca3b4ab2c0c9ea79780
Dietmar Stein <info@simracingjustfair.org>
2024-03-09 16:15:50 +0100

    SteamIDs can be given by $args

--- rfactor2/pwsh/gui/ws_installer.ps1
+++ rfactor2/pwsh/gui/ws_installer.ps1
@@ -1,15 +1,31 @@
-﻿. ./variables.ps1
+﻿#
+# simple script in order to install workshop items
+#
+# Dietmar Stein, 03/2024, info@simracingjustfair.org
+#
 
-$STEAMIDS="2362626369", "2648433641", "2962890868", "553365002"
+# source variables
+. ./variables.ps1
 
- foreach ($STEAMID in $STEAMIDS)
- {    
-    write-host "Downloading SteamID "$STEAMID 
-    $ARGUMENTS=" +force_install_dir $RF2ROOT +login anonymous +workshop_download_item 365960 $STEAMID +quit"
+# getting SteamIDs by simply using $args
+$STEAMIDS=$args
+
+# what to do with the given IDs
+foreach ($STEAMID in $STEAMIDS)
+ {  
+    # simple message  
+    write-host "Downloading SteamID "$STEAMID
+
+    # generating arguments string
+    $ARGUMENTS=" +force_install_dir ""$RF2ROOT"" +login anonymous +workshop_download_item 365960 $STEAMID +quit"
+    
+    # downloading the workshop item
     start-process "$STEAMINSTALLDIR\steamcmd" -ArgumentList $ARGUMENTS -NoNewWindow -wait
 
-
+    # looking for RFCMP to install (need to be sorted, think of GT3 vehicles, 3.60 base, 3.61 update)
     $RFCMPS=(gci $RF2WORKSHOPPKGS\$STEAMID *.rfcmp -recurse| select -Expand Name|sort)
+    
+    # install each RFCMP with modmgr ... assuming modmgr is configured
     foreach ($RFCMP in $RFCMPS)
     {
         #& "$RF2ROOT\bin64\ModMgr.exe" -i"$RFCMP" -p"$RF2WORKSHOPPKGS\$STEAMID" -d"$RF2ROOT"
